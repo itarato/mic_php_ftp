d21bf527a5ab7d1e6753e23c7be14893273bbf3d
Peter Arato <it.arato@gmail.com>
2016-07-11 23:33:41 +0800

    Add doc.

--- src/Connection.php
+++ src/Connection.php
@@ -24,6 +24,13 @@ class Connection {
    */
   private $ftpResource;
 
+  /**
+   * Connection constructor.
+   *
+   * @param $host
+   * @param int $port
+   * @param int $timeout
+   */
   public function __construct($host, $port = 21, $timeout = 90) {
     $this->host = $host;
     $this->port = $port;
@@ -34,6 +41,10 @@ class Connection {
     if ($this->ftpResource) ftp_close($this->ftpResource);
   }
 
+  /**
+   * @return $this
+   * @throws \itarato\MicPhpFtp\ConnectionException
+   */
   public function connect() {
     if (!($this->ftpResource = @ftp_connect($this->host, $this->port, $this->timeout))) {
       throw new ConnectionException("Cannot connect to {$this->host}:{$this->port}.");
@@ -42,6 +53,12 @@ class Connection {
     return $this;
   }
 
+  /**
+   * @param $userName
+   * @param $password
+   * @return $this
+   * @throws \itarato\MicPhpFtp\ConnectionException
+   */
   public function login($userName, $password) {
     if (!ftp_login($this->ftpResource, $userName, $password)) {
       throw new ConnectionException('Cannot login.');
@@ -50,12 +67,29 @@ class Connection {
     return $this;
   }
 
+  /**
+   * @param string $dir
+   * @return string[]
+   */
   public function ls($dir = './') { return ftp_nlist($this->ftpResource, $dir); }
 
+  /**
+   * @param $dir
+   * @return bool
+   */
   public function cd($dir) { return ftp_chdir($this->ftpResource, $dir); }
 
+  /**
+   * @return string
+   */
   public function pwd() { return ftp_pwd($this->ftpResource); }
 
-  public function get($src, $dest, $mode = FTP_BINARY) { ftp_get($this->ftpResource, $dest, $src, $mode); }
+  /**
+   * @param $src
+   * @param $dest
+   * @param $mode
+   * @return bool
+   */
+  public function get($src, $dest, $mode = FTP_BINARY) { return ftp_get($this->ftpResource, $dest, $src, $mode); }
 
 }
